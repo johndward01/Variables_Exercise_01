85032f3ef89599a799ee5bfb750778c006bf663a
johndward01 <johndward01@gmail.com>
2021-09-27 11:14:58 -0500

    removed floating point table replaced with Microsoft Docs Link

--- Variables Exercise 01/Program.cs
+++ Variables Exercise 01/Program.cs
@@ -41,9 +41,12 @@ namespace Variables_Exercise_01
             long _long;                 // 64 bit	-9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
             ulong _ulong;               // 64 bit	                         0 to 18,446,744,073,709,551,615
 
-            float _float;               // 32 bit	               -1.5 x 1045 to 3.4 x 1038
-            double _double;             // 64 bit	                -5 x 10324 to 1.7 x 10308
-            decimal _decimal;           // 128 bit	                     -1028 to 7.9 x 1028
+
+            // Floating point types https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/floating-point-numeric-types
+            float _float;               
+            double _double;             
+            decimal _decimal;           
+
 
             char _char;                 // 16 bit	                         0 to 65535
             bool _bool;                 // ~1 bit	                      True or false
