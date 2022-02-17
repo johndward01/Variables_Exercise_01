ef3b3bdd6f0917c345bea30541b61d7b02b963d6
johndward <johndward01@gmail.com>
2022-02-17 17:17:55 -0600

    update

--- Variables Exercise 01/Program.cs
+++ Variables Exercise 01/Program.cs
@@ -4,75 +4,28 @@ namespace Variables_Exercise_01
 {
     class Program
     {
-        public struct PointA // A Value Type Container with a parameterized constructor
-        {
-            private double X;
-            private double Y;
-
-            public PointA(double x, double y) 
-            {
-                X = x;
-                Y = y;
-            }
-        }
-        public struct PointB // A Value Type Container with default constructor
-        {
-            public double X { get; set; }
-            public double Y { get; set; }
-        } 
-
-        public enum MyEnum { Sun, Mon, Tue, Wed, Thu, Fri, Sat }; // A Value Type Collection
-
         static void Main(string[] args)
         {
-            var myList = new List<object>();
-
             // - Complete all of the TODO's for this exercise
             // - Once finished save, commit, and then push to GitHub!
 
-            #region List of C# Value Types https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/value-types
-            //  DATATYPES         |       Size         |                     VALUES                          |
-            sbyte _sbyte;               // 8 bit	                      -128 to 127
-            byte _byte;                 // 8 bit	                         0 to 255
-            short _short;               // 16 bit	                   -32,768 to 32,767
-            ushort _ushort;             // 16 bit	                         0 to 65,535
-            int _int;                   // 32 bit	            -2,147,483,648 to 2,147,483,647
-            uint _uint;                 // 32 bit	                         0 to 4,294,967,295
-            long _long;                 // 64 bit	-9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
-            ulong _ulong;               // 64 bit	                         0 to 18,446,744,073,709,551,615
+            // TODO: Declare 3 constansts
 
-
-            // Floating point types https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/floating-point-numeric-types
-            float _float;               
-            double _double;             
-            decimal _decimal;           
-
-
-            char _char;                 // 16 bit	                         0 to 65535
-            bool _bool;                 // ~1 bit	                      True or false
+            #region Types
+            // string
+            // char
+            // bool
+            // int
+            // double
+            // decimal
             #endregion
 
-            // TODO: Declare 5 constansts
-            
+            // TODO: Declare a variable for each of the types above
+            // Make sure to use C# naming convention best practices 
 
-            // TODO: Declare 2 variables of each type from the list above (1 pair for each) 
-            // Make sure to use C# naming convention best practices (camelCase, PascalCase, etc...)
+            // TODO: Initialize each of the variables with a value
 
-
-
-
-            // TODO: Initialize 1 of the variables with that datatypes minimum value
-            // TODO: Initialize the other variable with that datatypes maximum value
             // TODO: Write out each of the variables to the Console 
-
-
-
-
-            // **BONUS**
-            // TODO: Create your own enum and initialize it with constants of your choosing
-            // TODO: Declare it and then iterate through it writing out to the console            
-            // TODO: Instantiate a struct and initialize all of it's members
-
         }
 
     }
